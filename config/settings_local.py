2bfd09cd81415e961833e588086ef2296cdfc395
whereismyjetpack <djb44@psu.edu>
2021-11-19 10:29:00 -0500

    adds logging to settings_local

--- config/settings_local.py
+++ config/settings_local.py
@@ -1,5 +1,7 @@
 import os
 import urllib
+import sys
+import logging
 
 from .settings_base import *
 
@@ -110,4 +112,29 @@ Q_CLUSTER = {
         'errors': 'strict',
         'unix_socket_path': None
     }
+}
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'formatters': {
+       'verbose': {
+           'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+       },
+   },
+   'handlers': {
+       'console': {
+           'level': 'INFO',
+           'class': 'logging.StreamHandler',
+           'stream': sys.stdout,
+           'formatter': 'verbose'
+       },
+   },
+   'loggers': {
+       '': {
+           'handlers': ['console'],
+           'level': 'INFO',
+           'propagate': True,
+       },
+   },
 }
