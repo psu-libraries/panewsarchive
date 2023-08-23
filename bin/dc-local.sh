a54bc0f56110a852cd0d9483b3b45376daac534b
Jim Campbell <jkc103@psu.edu>
2023-08-23 16:07:37 -0400

    APPINT-7485 panewsarchive running in docker-compose

--- bin/dc-local.sh
+++ bin/dc-local.sh
@@ -2,7 +2,7 @@
 # startup in local mode when using docker-compose
 export ONI_DEBUG=1
 
-python manage.py setup_index
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python /open-oni/manage.py setup_index
+python /open-oni/manage.py migrate
+python /open-oni/manage.py runserver 0.0.0.0:8080
 
